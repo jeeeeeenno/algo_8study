1ee3ba3ffa2d80ba427911874af3432a5238bbd0
soyi <myeongsoyi@gmail.com>
2023-08-17 22:11:59 +0900

    명소이 : SWEA1222 계산기1 수정

--- APS/230817_Stack/1222_계산기1/명소이_계산기1.java
+++ APS/230817_Stack/1222_계산기1/명소이_계산기1.java
@@ -1,20 +1,55 @@
 package 계산기1;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Solution {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) throws FileNotFoundException {
+//		Scanner scanner = new Scanner(System.in);
+		Scanner scanner = new Scanner(new File("./src/계산기1/input.txt"));
+		Map<Character, Integer> priority = new HashMap<>();
+		priority.put('+', 0);
 		for(int test = 1; test <= 10; test++) {
-			int len = scanner.nextInt(); // 식의 길이
-			String oper = scanner.next();
-			int total = 0;
+			// 입력
+			int len = scanner.nextInt(); // 길이
+			char[] expression = scanner.next().toCharArray(); // 연산식
+			// 후위 표현식으로 변경
+			ArrayList<Character> postfix = new ArrayList<>(); // 후위 표현식
+			Stack<Character> operator = new Stack<>(); // 연산자
 			for(int i = 0; i < len; i++) {
-				if(oper.charAt(i) != '+') {
-					total += oper.charAt(i)-'0';
+				if(expression[i] >= '0' && expression[i] <= '9') { // 피연산자인 경우
+					postfix.add(expression[i]);
+				}else if(operator.empty()) {
+					operator.add(expression[i]);
+				}else if(priority.get(expression[i]) >= priority.get(operator.peek())) {
+					operator.add(expression[i]);
+				}else {
+					postfix.add(operator.pop());
 				}
 			}
-			System.out.printf("#%d %d\n", test, total);
+			while(!operator.empty()) {
+				postfix.add(operator.pop());
+			}
+			// 계산하기
+			Stack<Integer> nums = new Stack<>(); // 피연산자
+			for(int i = 0; i < len; i++) {
+				if(Character.isDigit(postfix.get(i))) {
+					nums.add(postfix.get(i)-'0');
+				}else {
+					if(postfix.get(i)=='+') {
+						int B = nums.pop();
+						int A = nums.pop();
+						nums.add(A+B);
+					}
+				}
+			}
+			// 출력
+			System.out.printf("#%d %d\n", test, nums.pop());
 		}
 		scanner.close();
 	}
