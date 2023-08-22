8366806b4e075e779c892c2781cbbd248a0139c4
myeongsoyi <66073336+myeongsoyi@users.noreply.github.com>
2023-08-22 23:42:40 +0900

    명소이 : 퍼펙트 셔플 오류 수정

--- APS/230818_Queue/3499_퍼펙트셔플/명소이_퍼펙트셔플.java
+++ APS/230818_Queue/3499_퍼펙트셔플/명소이_퍼펙트셔플.java
@@ -1,5 +1,40 @@
-package ∞˙¡¶0818;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
-public class ∏Ìº“¿Ã_∆€∆Â∆Æº≈«√ {
-
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for(int test = 1; test <= T; test++) {
+			// ÏûÖÎ†•
+			int N = sc.nextInt()+1;
+			Queue<String> deck1 = new LinkedList<>();
+			Queue<String> deck2 = new LinkedList<>();
+			for(int i = 0; i < N/2; i++) {
+				deck1.add(sc.next());
+			}
+			for(int i = N/2; i < N-1; i++) {
+				deck2.add(sc.next());
+			}
+			// ÏÖîÌîå
+			Queue<String> cards = new LinkedList<>();
+			while(true) {
+				if(deck1.isEmpty())break;
+				cards.add(deck1.remove());
+				if(deck2.isEmpty()) break;
+				cards.add(deck2.remove());
+				
+			}
+			// Ï∂úÎ†•
+			System.out.printf("#%d ", test);
+			for(int i = 0; i < cards.size(); i++) {
+				String tmp = cards.remove();
+				System.out.printf("%s ", tmp);
+				cards.add(tmp);
+			}
+			System.out.println();
+		}
+		sc.close();
+	}
 }
