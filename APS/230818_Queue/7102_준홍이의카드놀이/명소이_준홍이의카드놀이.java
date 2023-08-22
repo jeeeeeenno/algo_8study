43f2f130548061327a6dd573aa6621bd98e11cd2
myeongsoyi <66073336+myeongsoyi@users.noreply.github.com>
2023-08-22 23:44:57 +0900

    명소이 : 준홍이의 카드 놀이 오류 수정

--- APS/230818_Queue/7102_준홍이의카드놀이/명소이_준홍이의카드놀이.java
+++ APS/230818_Queue/7102_준홍이의카드놀이/명소이_준홍이의카드놀이.java
@@ -1,5 +1,28 @@
-package 과제0818;
+import java.util.Scanner;
 
-public class 명소이_준홍이의카드놀이 {
-
+public class Solution {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int T = scanner.nextInt();
+		for(int test = 1; test <= T; test++) {
+			int N = scanner.nextInt();
+			int M = scanner.nextInt();
+			int ans = 0;
+			int[] sum = new int[N+M+1];
+			for(int i = 1; i <= N; i++) {
+				for(int j = 1; j <= M; j++) {
+					sum[i+j]++;
+					ans = ans > sum[i+j] ? ans : sum[i+j];
+				}
+			}
+			System.out.printf("#%d ", test);
+			for(int i = 1; i <= N+M; i++) {
+				if(sum[i] == ans) {
+					System.out.printf("%d ", i);
+				}
+			}
+			System.out.println();
+		}
+		scanner.close();
+	}
 }
